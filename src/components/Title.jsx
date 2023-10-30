20652b83b29fef74ccb7e7fb29f7d46c4202b9a3
prettyangela <angelala6600@gmail.com>
2023-10-30 21:01:01 +0700

    create title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="col-md-12 mt-4">
+        <div className="text-center pb-5">
+            <h1>What's the plan for today?</h1>
+        </div>
+    </div>
+
+  )
+}
+
+export default Title
